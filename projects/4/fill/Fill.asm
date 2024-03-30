4967483ed9e491390bc5b8815dc64e4cbe89a0e6
dhruv <dhruvpatel1234.dp39@gmail.com>
2024-03-30 19:18:57 +0530

    Completed Week 4 Task: Wrote machine language code for multiplying two numbers and blackening the screen on keypress. The code is written as .asm files in project4.zip

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,91 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//Set length of array n = SCREEN-KBD
+@KBD
+D=A
+
+@n
+M=D
+
+@SCREEN
+D=A
+
+@n
+M=M-D
+
+//Set incrementer for as 0
+@i
+M=0
+
+//Infinite loop to check keypress
+(CHECK)
+@KBD
+D=M
+@BLACK
+D;JNE
+@WHITE
+D;JEQ
+@CHECK
+0;JMP
+
+(BLACK)
+//Reset incrementer
+@i
+M=0
+//if (i==n) goto CHECK
+(BLOOP)
+@n
+D=M
+@i
+D=D-M
+@CHECK
+D;JEQ
+
+//RAM[SCREEN+i]=-1
+@SCREEN
+D=A
+@i
+A=D+M
+M=-1
+
+//i++
+@i
+M=M+1
+
+@BLOOP
+0;JMP
+
+(WHITE)
+//Reset incrementer
+@i
+M=0
+
+//if (i==n) goto CHECK
+(WLOOP)
+@n
+D=M
+@i
+D=D-M
+@CHECK
+D;JEQ
+
+//RAM[SCREEN+i]=0
+@SCREEN
+D=A
+@i
+A=D+M
+M=0
+
+//i++
+@i
+M=M+1
+
+@WLOOP
+0;JMP
+
+(END)
+@END
+0;JMP
+
+
